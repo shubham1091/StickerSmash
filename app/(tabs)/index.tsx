ef6af6426c062addf88c0507f02f47885ffcd34e
shubham <shubhamverma1091@gmail.com>
2024-11-06 00:34:34 +0530

    adding navigation

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link
+        href={"/about"}
+        style={styles.button}
+      >
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
