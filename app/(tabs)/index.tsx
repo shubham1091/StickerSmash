dc527082d6b084495da39ab59f9df95fa8f5911b
shubham <shubhamverma1091@gmail.com>
2024-11-06 12:26:07 +0530

    handling platform differences

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,9 +9,10 @@ import { type ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 import { useRef, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 const Index = () => {
@@ -54,18 +55,34 @@ const Index = () => {
     setIsModalVisible(false);
   };
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-      await MediaLibrary.saveToLibraryAsync(localUri);
-
-      if (localUri) {
-        alert("Saved to library");
+    if (Platform.OS === "web") {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+
+        if (localUri) {
+          alert("Saved to library");
+        }
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
   return (
