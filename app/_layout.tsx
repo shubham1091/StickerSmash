78b7acbdce1fa2b7b33fe0be3f390f5ca7ab8a68
shubham <shubhamverma1091@gmail.com>
2024-11-06 12:36:12 +0530

    configured splashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
 import { Stack } from "expo-router";
+// import * as SplashScreen from "expo-splash-screen";
+import { setStatusBarStyle } from "expo-status-bar";
+import { useEffect } from "react";
+
 
 export default function RootLayout() {
+  // SplashScreen.preventAutoHideAsync();
+  // setTimeout(SplashScreen.hideAsync, 5000)
+  useEffect(() => {
+    setTimeout(() => {
+      setStatusBarStyle("light");
+    }, 0);
+  }, []);
   return (
     <Stack>
       <Stack.Screen
